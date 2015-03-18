905bcbc56ee32406061e583e10db1d0cf6260d65
Eduardo Medina <emedinaa@gmail.com>
2015-03-17 20:17:01 -0500

    fragments y activities

---
+++ Lesson1/AppLesson1/app/src/main/java/clom/isil/am2/applesson1/HomeActivity.java
@@ -0,0 +1,79 @@
+package clom.isil.am2.applesson1;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            return rootView;
+        }
+    }
+
+    private void changeFragment(Bundle args, Fragment fragment,String fragName) {
+        // update the main content by replacing fragments
+        if (args != null) fragment.setArguments(args);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragName != null)
+        {
+            fragmentManager.beginTransaction().replace(R.id.container, fragment,fragName).commit();
+        }
+
+    }
+}
---
+++ Lesson1/AppLesson1/app/src/main/java/clom/isil/am2/applesson1/view/listener/OnFragmentListener.java
@@ -0,0 +1,10 @@
+package clom.isil.am2.applesson1.view.listener;
+
+/**
+ * Created by emedinaa on 17/03/2015.
+ */
+public interface OnFragmentListener {
+
+    void exitApp();
+
+}
