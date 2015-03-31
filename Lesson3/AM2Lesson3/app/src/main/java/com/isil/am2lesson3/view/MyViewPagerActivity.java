3cdcaefd3fb0687864fa5aa239dadd1d0d4f3ffb
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-03-31 17:09:07 -0500

    view pager

---
+++ Lesson3/AM2Lesson3/app/src/main/java/com/isil/am2lesson3/view/MyViewPagerActivity.java
@@ -0,0 +1,116 @@
+package com.isil.am2lesson3.view;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.isil.am2lesson3.R;
+import com.isil.am2lesson3.view.fragments.AFragment;
+import com.isil.am2lesson3.view.fragments.BFragment;
+import com.isil.am2lesson3.view.fragments.CFragment;
+import com.isil.am2lesson3.view.listener.OnFragmentListener;
+import com.isil.am2lesson3.view.transforms.ZoomOutPageTransformer;
+
+public class MyViewPagerActivity extends ActionBarActivity implements OnFragmentListener {
+
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 3;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_view_pager);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_view_pager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            Bundle data = new Bundle();
+            switch(position){
+
+                /** tab1 is selected */
+                case 0:
+                    AFragment fragment1 =AFragment.newInstance(null,null);
+                    return fragment1;
+
+                /** tab2 is selected */
+                case 1:
+                    BFragment fragment2 =BFragment.newInstance(null,null);
+                    return fragment2;
+                case 2:
+                    CFragment fragment3 =CFragment.newInstance(null,null);
+                    return fragment3;
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
