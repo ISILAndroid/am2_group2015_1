318f62b367811115c4223c2fdf3803d419eb6ae5
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-04-14 16:39:49 -0500

    *

--- Lesson5/ExampleListViewAdapter/app/src/main/java/com/isil/am2/examplelistviewadapter/HomeActivity1.java
+++ Lesson5/ExampleListViewAdapter/app/src/main/java/com/isil/am2/examplelistviewadapter/HomeActivity1.java
@@ -20,9 +20,8 @@ public class HomeActivity1 extends ActionBarActivity  implements OnProductListen
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_activity1, menu);
-        return true;
+
+        return false;
     }
 
     @Override
@@ -30,14 +29,8 @@ public class HomeActivity1 extends ActionBarActivity  implements OnProductListen
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     @Override
--- Lesson5/ExampleListViewAdapter/app/src/main/java/com/isil/am2/examplelistviewadapter/HomeActivity2.java
+++ Lesson5/ExampleListViewAdapter/app/src/main/java/com/isil/am2/examplelistviewadapter/HomeActivity2.java
@@ -20,24 +20,13 @@ public class HomeActivity2 extends ActionBarActivity implements OnProductListene
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_activity2, menu);
-        return true;
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     @Override
