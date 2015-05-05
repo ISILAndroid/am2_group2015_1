c2d90ef5ec35443576ee076a28c374ce2235bda1
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-05-05 13:02:18 -0500

    * parcial

---
+++ EjemploParcial/app/src/main/java/com/isil/parcial/HomeActivity.java
@@ -0,0 +1,61 @@
+package com.isil.parcial;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.isil.parcial.view.NavigationDrawerActivity;
+import com.isil.parcial.view.listener.OnFragmentListener;
+
+
+public class HomeActivity extends ActionBarActivity implements OnFragmentListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        findViewById(R.id.btnIngresar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoHome();
+            }
+        });
+
+    }
+
+    private void gotoHome()
+    {
+        startActivity(new Intent(this, NavigationDrawerActivity.class));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return false;
+    }
+
+    @Override
+    public void gotoAction(Object obj)
+    {
+
+    }
+
+
+}
---
+++ EjemploParcial/app/src/main/java/com/isil/parcial/view/listener/OnFragmentListener.java
@@ -0,0 +1,8 @@
+package com.isil.parcial.view.listener;
+
+/**
+ * Created by emedinaa on 31/03/15.
+ */
+public interface OnFragmentListener {
+    void gotoAction(Object obj);
+}
