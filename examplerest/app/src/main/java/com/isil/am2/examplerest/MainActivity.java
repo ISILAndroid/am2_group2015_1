e79f2c6cb03eed0dec6e7a0bcf31909466f80cc5
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-05-19 18:24:46 -0500

    no message

--- examplerest/app/src/main/java/com/isil/am2/examplerest/MainActivity.java
+++ examplerest/app/src/main/java/com/isil/am2/examplerest/MainActivity.java
@@ -34,8 +34,8 @@ public class MainActivity extends ActionBarActivity implements OnFragmentListene
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+        return false;
     }
 
     @Override
@@ -43,14 +43,9 @@ public class MainActivity extends ActionBarActivity implements OnFragmentListene
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     /**
