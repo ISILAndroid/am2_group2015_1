ef98193514f0b8c0d8891e19629e7c29537b84f2
Eduardo Medina <emedinaa@gmail.com>
2015-05-26 20:08:49 -0500

    pruebas GET

---
+++ Examplerest/app/src/main/java/com/isil/am2/examplerest/MainActivity.java
@@ -0,0 +1,73 @@
+package com.isil.am2.examplerest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import com.isil.am2.examplerest.view.fragments.MainFragment;
+import com.isil.am2.examplerest.view.fragments.MascotaFragment;
+import com.isil.am2.examplerest.view.listeners.OnFragmentListener;
+
+
+public class MainActivity extends ActionBarActivity implements OnFragmentListener{
+
+    private MainFragment mainFragment= MainFragment.newInstance(null,null);
+    private MascotaFragment mascotaFragment= MascotaFragment.newInstance(null,null);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, mascotaFragment)
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        return false;
+    }
+
+    @Override
+    public void changeFragment(Bundle bundle, String fragName, String fragBack) {
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
---
+++ Examplerest/app/src/main/java/com/isil/am2/examplerest/view/listeners/OnFragmentListener.java
@@ -0,0 +1,11 @@
+package com.isil.am2.examplerest.view.listeners;
+
+import android.os.Bundle;
+
+/**
+ * Created by emedinaa on 19/05/15.
+ */
+public interface OnFragmentListener {
+
+    void changeFragment(Bundle bundle , String fragName, String fragBack);
+}
